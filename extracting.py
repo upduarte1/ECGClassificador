1d0ce52ca17a2a3494df1104318a61ad8f1d433d
upduarte1 <up202004559@up.pt>
2025-10-28 18:32:42 +0100

    extract and connect

--- extracting.py
+++ extracting.py
@@ -1,14 +1,17 @@
 import pandas as pd
-import json
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import streamlit as st
 
-def get_signal_by_id(signal_id: int):
+def get_signal_by_id(signal_id: int, df_ecg: pd.DataFrame = None):
+
+    if df_ecg is None:
+        raise ValueError("DataFrame df_ecg must be provided.")
+    
     row = df_ecg[df_ecg["signal_id"] == signal_id]
+
     if row.empty:
         raise ValueError(f"Signal with ID {signal_id} not found.")
+    
     ecg_str = row.iloc[0]["ecg_signal"]
     heart_rate = float(row.iloc[0]["heart_rate"])
     values = [float(v.strip()) for v in str(ecg_str).split(",") if v.strip() not in ("", "-")]
+    
     return values, heart_rate
